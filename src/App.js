d8a9f48f51cd7f32ddd296815e19527e3cf942b9
Benjamin Michel <bmichel@tilli.fr>
2017-03-07 23:15:10 +0100

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 
-class App extends Component {
+// Make sure you swap this out with your Firebase app's config
+const config = {
+  apiKey: "AIzaSyAwMhCHX0yScEXvFpSFgSDU06Rr93o_Y3k",
+  authDomain: "where-are-you-a5111.firebaseapp.com",
+  databaseURL: "https://where-are-you-a5111.firebaseio.com",
+  storageBucket: "where-are-you-a5111.appspot.com",
+  messagingSenderId: "39848861470"
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    const fb = firebase.initializeApp(config);
+    this.db = fb.database();
+  }
+
+  add() {
+    const rand = Math.random();
+    this.db.ref('users/2').set({
+      username: `name${rand}`,
+      email: 'email2',
+    })
+  }
+
+  componentDidMount() {
+    this.db.ref('users/2').on('value', snapshot => {
+      const store = snapshot.val();
+      this.setState({ store })
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>My Prototype</h1>
+        <p>{JSON.stringify(this.state.store)}</p>
+        <p onClick={() => this.add()}>Add</p>
       </div>
     );
   }
