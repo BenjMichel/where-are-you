5b74e316f637faddaaeb24af52e38452cb4bda3f
Benjamin Michel <bmichel@tilli.fr>
2017-03-13 01:31:26 +0100

    Find a colleague

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import * as firebase from 'firebase';
-import TextField from 'material-ui/TextField';
 import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
+import AutoComplete from 'material-ui/AutoComplete';
 import Dialog from 'material-ui/Dialog';
-import SelectField from 'material-ui/SelectField';
+import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import TextField from 'material-ui/TextField';
 
 import QRScanner from './QRScanner';
 
@@ -26,28 +27,30 @@ const style = {
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { username: '' };
+    this.state = { username: '', dataSource: [] };
 
     const fb = firebase.initializeApp(config);
     this.db = fb.database();
 
     this.openCheckInModal = this.openCheckInModal.bind(this);
+    this.openFindModal = this.openFindModal.bind(this);
     this.closeCheckInModal = this.closeCheckInModal.bind(this);
     this.checkIn = this.checkIn.bind(this);
+    this.handleFindInput = this.handleFindInput.bind(this);
   }
 
   componentDidMount() {
     this.db.ref().on('value', (snapshot) => {
       const store = snapshot.val();
-      this.setState({ store });
+      this.setState({ userPositions: this.formatFirebaseData(store) });
     });
   }
 
-  add() {
-    const rand = Math.random();
-    this.db.ref(`users/${this.state.username}`).set({
-      value: `name${rand}`,
-      username: this.state.username,
+  formatFirebaseData(store = { users: {} }) {
+    const { users } = store;
+    return Object.keys(users).map((key) => {
+      const user = users[key];
+      return `${user.username} - ${user.room}`;
     });
   }
 
@@ -62,20 +65,103 @@ class App extends React.Component {
   checkIn(room) {
     this.db.ref(`users/${this.state.username}`).set({
       room,
-      username: this.state.username,
+      username: this.state.username || 'Anne Onime',
       date: (new Date()).toString(),
     });
     this.closeCheckInModal();
   }
 
-  render() {
+  openFindModal() {
+    this.setState({ openFindModal: true });
+  }
+
+  handleFindInput(input) {
+    this.setState({ dataSource: this.state.userPositions.filter(userPosition => userPosition.search(input) !== -1) });
+  }
+
+  renderCheckInModal() {
     const actions = [
       <FlatButton
-        label="Cancel"
+        label="Annuler"
         primary
         onTouchTap={this.closeCheckInModal}
       />,
     ];
+    return (
+      <Dialog
+        title="Indiquer où je suis"
+        actions={actions}
+        modal={false}
+        open={this.state.openCheckInModal}
+        onRequestClose={this.handleClose}
+        autoScrollBodyContent
+      >
+        {!this.state.mode &&
+          <div>
+            <RaisedButton label="Scanner un QR code" secondary style={style} onClick={() => this.setState({ mode: 'qrcode' })} />
+            <RaisedButton label="Manuellement" primary style={style} onClick={() => this.setState({ mode: 'manual' })} />
+          </div>
+        }
+        {this.state.mode === 'manual' &&
+          <div>
+            <SelectField
+              floatingLabelText="Aile"
+              value={this.state.room}
+              onChange={(event, index, value) => this.setState({ room: value })}
+              autoWidth
+              maxHeight={200}
+            >
+              <MenuItem value="Rouge - B1" primaryText="Rouge - B1" />
+              <MenuItem value="Rouge - B2" primaryText="Rouge - B2" />
+              <MenuItem value="Rouge - B3" primaryText="Rouge - B3" />
+              <MenuItem value="Bibliothèque" primaryText="Bibliothèque" />
+              <MenuItem value="Mantik" primaryText="Mantik" />
+              <MenuItem value="Café" primaryText="Café/cuisine" />
+              <MenuItem value="B4" primaryText="B4" />
+              <MenuItem value="Cafet" primaryText="Cafet" />
+              <MenuItem value="Créa" primaryText="Salle créa" />
+              <MenuItem value="B5" primaryText="Vert - B5" />
+              <MenuItem value="SAV" primaryText="SAV" />
+              <MenuItem value="other" primaryText="Ailleurs/télétravail" />
+              <MenuItem value="VIP" primaryText="Salle VIP" />
+              <MenuItem value="wood" primaryText="Salles réunion Wood" />
+              <MenuItem value="white" primaryText="Salle réunion white" />
+            </SelectField>
+            <RaisedButton label="Valider" primary style={style} onClick={() => this.checkIn(this.state.room)} />
+          </div>
+        }
+        {this.state.mode === 'qrcode' && <QRScanner onScan={this.checkIn} />}
+      </Dialog>
+    );
+  }
+
+  renderFindModal() {
+    const actions = [
+      <FlatButton
+        label="Annuler"
+        primary
+        onTouchTap={() => this.setState({ openFindModal: false })}
+      />,
+    ];
+    return (
+      <Dialog
+        title="Indiquer où je suis"
+        actions={actions}
+        modal={false}
+        open={this.state.openFindModal}
+        onRequestClose={this.handleClose}
+        autoScrollBodyContent
+      >
+        <AutoComplete
+          hintText="Entre le nom d'un Ozien"
+          dataSource={this.state.dataSource}
+          onUpdateInput={this.handleFindInput}
+        />
+      </Dialog>
+    );
+  }
+
+  render() {
     return (
       <div>
         <AppBar title="The OZ - Where R U ?" />
@@ -89,53 +175,10 @@ class App extends React.Component {
         />
         <div>
           <RaisedButton label="Indiquer où je suis" primary style={style} onClick={this.openCheckInModal} />
-          <RaisedButton label="Rechercher quelqu'un" secondary style={style} />
+          <RaisedButton label="Rechercher quelqu'un" secondary style={style} onClick={this.openFindModal} />
         </div>
-
-        <Dialog
-          title="Indiquer où je suis"
-          actions={actions}
-          modal={false}
-          open={this.state.openCheckInModal}
-          onRequestClose={this.handleClose}
-          autoScrollBodyContent
-        >
-          {!this.state.mode &&
-            <div>
-              <RaisedButton label="Scanner un QR code" secondary style={style} onClick={() => this.setState({ mode: 'qrcode' })} />
-              <RaisedButton label="Manuellement" primary style={style} onClick={() => this.setState({ mode: 'manual' })} />
-            </div>
-          }
-          {this.state.mode === 'manual' &&
-            <div>
-              <SelectField
-                floatingLabelText="Aile"
-                value={this.state.room}
-                onChange={(event, index, value) => this.setState({ room: value })}
-                autoWidth
-                maxHeight={200}
-              >
-                <MenuItem value="Rouge - B1" primaryText="Rouge - B1" />
-                <MenuItem value="Rouge - B2" primaryText="Rouge - B2" />
-                <MenuItem value="Rouge - B3" primaryText="Rouge - B3" />
-                <MenuItem value="Bibliothèque" primaryText="Bibliothèque" />
-                <MenuItem value="Mantik" primaryText="Mantik" />
-                <MenuItem value="Café" primaryText="Café/cuisine" />
-                <MenuItem value="B4" primaryText="B4" />
-                <MenuItem value="Cafet" primaryText="Cafet" />
-                <MenuItem value="Créa" primaryText="Salle créa" />
-                <MenuItem value="B5" primaryText="Vert - B5" />
-                <MenuItem value="SAV" primaryText="SAV" />
-                <MenuItem value="other" primaryText="Ailleurs/télétravail" />
-                <MenuItem value="VIP" primaryText="Salle VIP" />
-                <MenuItem value="wood" primaryText="Salles réunion Wood" />
-                <MenuItem value="white" primaryText="Salle réunion white" />
-              </SelectField>
-              <RaisedButton label="Valider" primary style={style} onClick={() => this.checkIn(this.state.room)} />
-            </div>
-          }
-          {this.state.mode === 'qrcode' && <QRScanner onScan={this.checkIn} />}
-        </Dialog>
+        {this.renderCheckInModal()}
+        {this.renderFindModal()}
       </div>
     );
   }
